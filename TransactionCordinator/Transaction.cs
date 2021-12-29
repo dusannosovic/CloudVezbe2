b3fea56bff017a968cc26efa82dd2b9c6517006b
dusannosovic <dusanosovic@gmail.com>
2021-12-29 14:22:56 +0100

    Added tablestorage

--- TransactionCordinator/Transaction.cs
+++ TransactionCordinator/Transaction.cs
@@ -4,6 +4,10 @@ using Microsoft.ServiceFabric.Services.Client;
 using Microsoft.ServiceFabric.Services.Communication.Client;
 using Microsoft.ServiceFabric.Services.Communication.Wcf;
 using Microsoft.ServiceFabric.Services.Communication.Wcf.Client;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.WindowsAzure;
+using Microsoft.Azure;
 using ServiceBank;
 using System;
 using System.Collections.Generic;
@@ -44,17 +48,51 @@ namespace TransactionCordinator
 
             if (c)
             {
-                 c = await servicePartitionClient2.InvokeWithRetryAsync(client => client.Channel.Commit());
-                 book = await servicePartitionClient2.InvokeWithRetryAsync(client => client.Channel.Commit());
+                c = false;
+                book = false;
+                c = await servicePartitionClient2.InvokeWithRetryAsync(client => client.Channel.Commit());
+                book = await servicePartitionClient1.InvokeWithRetryAsync(client => client.Channel.Commit());
                 if (c && book)
                 {
-                    ;
+                    c = false;
+                    book = false;
+                    c = await servicePartitionClient2.InvokeWithRetryAsync(client => client.Channel.RollBack(true));
+                    book = await servicePartitionClient1.InvokeWithRetryAsync(client => client.Channel.RollBack(true));
+                    AddDataToTable(servicePartitionClient1, servicePartitionClient2);
+                }
+                else
+                {
+                    c = false;
+                    book = false;
+                    c = await servicePartitionClient2.InvokeWithRetryAsync(client => client.Channel.RollBack(false));
+                    book = await servicePartitionClient1.InvokeWithRetryAsync(client => client.Channel.RollBack(false));
                 }
                 
             }
             return true;
         }
+        public async Task AddDataToTable(ServicePartitionClient<WcfCommunicationClient<IBookStore>> servicePartitionClient1, ServicePartitionClient<WcfCommunicationClient<IBank>> servicePartitionClient2)
+        {
+            Dictionary<string, BooksData> books = await servicePartitionClient1.InvokeWithRetryAsync(client => client.Channel.GetBookData());
+            Dictionary<int, BankData> bankData = await servicePartitionClient2.InvokeWithRetryAsync(client => client.Channel.GetBankData());
+            CloudStorageAccount _storageAccount;
+            CloudTable _table;
+            _storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("DataConnectionString"));
+            CloudTableClient tableClient = new CloudTableClient(new Uri(_storageAccount.TableEndpoint.AbsoluteUri), _storageAccount.Credentials);
+            _table = tableClient.GetTableReference("StoreBankTable");
+            foreach(BooksData bk in books.Values)
+            {
+                TableOperation insertOperation = TableOperation.InsertOrReplace(new BookTableEntity(bk.NazivKnjige, bk.Cena, bk.BrojKnjiga));
+                _table.Execute(insertOperation);
+            }
+            foreach(BankData bk in bankData.Values)
+            {
+                TableOperation insertOperation = TableOperation.InsertOrReplace(new TransactionTableEntity(bk.Id,bk.Ime,bk.Prezime,bk.Iznos));
+                _table.Execute(insertOperation);
+            }
+            
 
+        }
         public void RollBack()
         {
             throw new NotImplementedException();
